c26832243d8ed51be306d81db3a50484683eb618
Kiran H <kiranhari1998@gmail.com>
2020-10-07 10:59:38 +0530

    First commit

---
+++ src/main/java/com/cognizant/WeatherTday/WeatherTdayApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.WeatherTday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherTdayApplication.class, args);
+	}
+
+}
